0e54e498d256798f18246d2d4d7dec0f11fa4680
JoanJuan10 <iijoanjuan10@gmail.com>
2020-12-28 21:27:52 +0100

    Ejercicio 12

---
+++ Ejercicios/Ejercicios/Ejercicio12.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicios
+{
+    class Ejercicio12
+    {
+        public void Ejercicio()
+        {
+            int[] vector = new int[10];
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] = i;
+            }
+            double media = 0;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                media += vector[i];
+            }
+            media /= vector.Length; 
+            Console.WriteLine("Media total: {0}", media);
+        }
+    }
+}
