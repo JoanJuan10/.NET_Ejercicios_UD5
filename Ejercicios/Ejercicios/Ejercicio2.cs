e88fe3c51cab38d9ad6062f273c7a2f137027232
JoanJuan10 <iijoanjuan10@gmail.com>
2020-12-28 11:57:32 +0100

    Ejercicio 2

---
+++ Ejercicios/Ejercicios/Ejercicio2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace Ejercicios
+{
+	public class Ejercicio2
+	{
+		public void Ejercicio()
+		{
+			Console.WriteLine("Numero minimo para la generación aleatoria");
+
+			string minimoString = Console.ReadLine();
+
+			Console.WriteLine("Numero maximo para la generación aleatoria");
+
+			string maximoString = Console.ReadLine();
+
+
+			try
+            {
+				int minimo = Convert.ToInt32(minimoString);
+				int maximo = Convert.ToInt32(maximoString);
+				numAleatorio(minimo, maximo);
+
+			}
+			catch (Exception)
+            {
+				Console.WriteLine("Numeros no validos");
+            }
+			
+
+
+		}
+		public int numAleatorio(int min, int max)
+        {
+			Random r = new Random();
+			int genRand = r.Next(min, max);
+			return 0;
+        }
+	}
+}
+
