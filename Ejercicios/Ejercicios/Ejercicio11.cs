f8bd6d9418d29ca405d276f195ee212d2776f1cd
JoanJuan10 <iijoanjuan10@gmail.com>
2020-12-28 21:27:40 +0100

    Ejercicio 11

---
+++ Ejercicios/Ejercicios/Ejercicio11.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicios
+{
+    class Ejercicio11
+    {
+        public void Ejercicio()
+        {
+            int[] vector = new int[10];
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] = i;
+            }
+            int suma = 0;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                suma += vector[i];
+            }
+            Console.WriteLine("Suma total: {0}", suma);
+        }
+    }
+}
