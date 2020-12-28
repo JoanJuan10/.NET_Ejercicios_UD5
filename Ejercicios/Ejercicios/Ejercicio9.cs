bbf06f19542609d88b94686bb01aaa8625311379
JoanJuan10 <iijoanjuan10@gmail.com>
2020-12-28 20:51:18 +0100

    Ejercicio 9

---
+++ Ejercicios/Ejercicios/Ejercicio9.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicios
+{
+    class Ejercicio9
+    {
+        public void Ejercicio()
+        {
+            int[] vector = new int[10];
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.WriteLine("Inserta el valor de la celda {0}", (i + 1));
+                vector[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.WriteLine(vector[i]);
+            }
+        }
+    }
+}
